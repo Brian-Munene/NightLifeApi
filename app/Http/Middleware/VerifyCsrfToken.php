b9f7b9da727330db4b6a755bc0511bf7e11fa7cb
Brian Munene <brianmunene69@gmail.com>
2018-07-10 22:40:51 +0300

    login and register

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,8 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/chillspot',
+        '/register',
+        '/login',
         '/review',
         '/owner'
     ];
